06e094b7ca5e26d6ff246a1767709f91dd2c08ad
Hitesh Parashar <lycos_hit@yahoo.com>
2022-12-30 13:04:08 +0530

    test

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import './header.scss';
 
 const Heading = ({text}) => {
-    return <h3 className='header'>{text}</h3>
-}
+    const [state, setState] = useState(1);
+
+    const person = {
+        name: 'hitesh',
+        age: 22
+    };
+
+
+    const value = useMemo(() => {
+        console.log('recomputed');
+        return state;
+    }, [person]);
+
+    return <h3 className='header' onClick={() => setState(state + 1)}>{text}:{value}</h3>;
+};
 
 export default Heading;
